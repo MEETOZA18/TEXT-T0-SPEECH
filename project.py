d8a0ae4c27946e9a5d6ab61a448dcaf48e1b6bfa
Meet Oza <40018473+MEETOZA18@users.noreply.github.com>
2018-09-27 21:19:58 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,24 @@
+#importing pandas
+
+import pandas as pd
+
+#with the help of pandas reas a text file 
+data = pd.read_csv("fliename.txt",header=None)
+
+#Just printing the text file
+print(data)
+
+
+#Using Google text-to-speech(gtts)
+
+from gtts import gTTS
+import os
+
+#en=english language
+language = 'en'
+myobj = gTTS(text=data,lang=language,slow=False)
+
+#saving the speech as a mp3 file.
+myobj.save("name.mp3")
+
+os.system("mgp321 welcome.mp3")
