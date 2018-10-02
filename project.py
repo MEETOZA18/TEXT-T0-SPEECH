ab925bbcec40added78d5cc0c1a9f7856711ccf6
rshrc <rishieric91@gmail.com>
2018-10-02 18:09:40 +0530

    Fixed .mp3 naming

--- project.py
+++ project.py
@@ -1,24 +1,24 @@
-#importing pandas
+# importing pandas
 
 import pandas as pd
 
-#with the help of pandas read a text file 
-data = pd.read_csv("fliename.txt",header=None)
+# with the help of pandas read a text file
+data = pd.read_csv("fliename.txt", header=None)
 
-#Just printing the text file
+# Just printing the text file
 print(data)
 
 
-#Using Google text-to-speech(gtts)
+# Using Google text-to-speech(gtts)
 
 from gtts import gTTS
 import os
 
-#en=english language
+# en=english language
 language = 'en'
-myobj = gTTS(text=data,lang=language,slow=False)
+myobj = gTTS(text=data, lang=language, slow=False)
 
-#saving the speech as a mp3 file.
+# saving the speech as a mp3 file.
 myobj.save("name.mp3")
 
-os.system("mgp321 welcome.mp3")
+os.system("mgp321 name.mp3")
