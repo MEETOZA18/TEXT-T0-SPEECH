ba774f6ca9f66d651a8f94b10d885fd57c2285c3
rshrc <rishieric91@gmail.com>
2018-10-03 14:50:58 +0530

    Proper Merge Conflicts Resolved

---
+++ text_to_speech.py
@@ -0,0 +1,34 @@
+from text_to_speech import TextToSpeech
+
+obj = TextToSpeech()
+obj.text_input()
+obj.create_gtts_object()
+obj.speech_save()
+obj.run()
+
+
+from gtts import gTTS
+import os
+import sys
+
+
+class TextToSpeech:
+
+    def __init__(self):
+        pass
+
+    def text_input(self):
+        self.text = input("Enter Text : ")
+        print("Text Entered")
+
+    def create_gtts_object(self):
+        self.gtts_object = gTTS(text=self.text, lang='en')
+        print("Object Made")
+
+    def speech_save(self):
+        os.system("rm speech.mp3")  # deleting file, not overriding
+        self.gtts_object.save('speech.mp3')
+        print("File Saved")
+
+    def run(self):
+        os.system("mpg123 speech.mp3")
