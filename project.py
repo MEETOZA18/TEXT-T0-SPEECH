d45322c7f0bb4ee27ed71b0bf6bca6e9becf72fc
Shobhit singh <singhshobhit2000@gmail.com>
2018-10-05 02:11:19 +0530

    Added a feature
    User can now choose between entering a string to converting in into mp3 and giving a text file to convert it's content into mp3

--- project.py
+++ project.py
@@ -2,11 +2,18 @@
 from gtts import gTTS
 import os
 
-# en=english language
-myobj = gTTS(text=input("Enter Text for Text to Speech : "),
-             lang='en')
+n=int(input('1. Enter text\n2.Select a text file to convert to audio \n'))
+
+if (n==1):
+        myobj = gTTS(text=input("Enter Text for Text to Speech : "),
+                        lang='en', slow=False)
+
+else:
+        data = open(input("Enter the name of the file :"),'rt' )
+        text1 = data.read()
+        print (text1)
+        myobj = gTTS(text=text1,lang='en',slow=False)
 
 # saving the speech as a mp3 file.
 myobj.save("name.mp3")
-
 os.system("mgp321 name.mp3")
